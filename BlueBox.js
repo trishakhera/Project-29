437b7b95c0a75e410bd2cbc985c809ffee859759
trishakhera <73061278+trishakhera@users.noreply.github.com>
2020-12-16 19:32:27 +0530

    Add files via upload

---
+++ BlueBox.js
@@ -0,0 +1,9 @@
+class BlueBox extends Box{
+    constructor(x,y){
+        super(x, y, 20, 30);
+    }
+    display(){
+     fill(85,136,238);
+     super.display();
+    }
+}
---
+++ Projectile.js
@@ -0,0 +1,28 @@
+class Projectile{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.4,
+            length:10,
+        }
+        this.pointB = pointB;
+        this.body = Constraint.create(options);
+        World.add(world, this.body);
+     }
+    
+    release(){
+         this.body.bodyA = null;
+    }
+    
+    display(){
+        if(this.body.bodyA){
+            var pointA = this.body.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(1);
+            stroke("grey");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+}
